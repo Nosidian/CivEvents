d2e328399d8a3d9e6ee5d82b1f85ffbab8539a21
BuildTools <unconfigured@null.spigotmc.org>
2024-12-29 20:21:07 -0500

    Version 1.20 & Higher

--- src/main/java/nos/civevents/CivAdmins/AdminPlayerData.java
+++ src/main/java/nos/civevents/CivAdmins/AdminPlayerData.java
@@ -2,6 +2,7 @@ package nos.civevents.CivAdmins;
 
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.model.user.User;
+import nos.civevents.CivEvents;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
@@ -12,32 +13,21 @@ import java.util.UUID;
 
 @SuppressWarnings("all")
 public class AdminPlayerData {
+    private CivEvents plugin;
     private LuckPerms luckPerms;
+    public AdminPlayerData(CivEvents plugin, LuckPerms luckPerms) {
+        this.plugin = plugin;
+        this.luckPerms = luckPerms;
+    }
     private static LuckPerms getLuckPerms() {
         return Bukkit.getServicesManager().load(LuckPerms.class);
     }
     static void clearAllPlayerData(CommandSender sender) {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            player.getInventory().clear();
-            player.getEnderChest().clear();
-            UUID playerUUID = player.getUniqueId();
-            File[] worldFolders = Bukkit.getWorlds().stream()
-                    .map(world -> world.getWorldFolder())
-                    .toArray(File[]::new);
-            for (File worldFolder : worldFolders) {
-                File advancementsFolder = new File(worldFolder, "advancements");
-                if (advancementsFolder.exists() && advancementsFolder.isDirectory()) {
-                    File playerAdvancementFile = new File(advancementsFolder, playerUUID + ".json");
-                    if (playerAdvancementFile.exists()) {
-                        if (playerAdvancementFile.delete()) {
-                            sender.sendMessage("§f§lCivEvents §f| §aDeleted advancement data for: " + player.getName());
-                        } else {
-                            sender.sendMessage("§f§lCivEvents §f| §cFailed to delete advancement data for: " + player.getName());
-                        }
-                    }
-                }
-            }
-            player.kickPlayer("§f§lCivEvents §f| §cYour data is being cleared");
+            clearPlayerDataForOnline(sender, player);
+        }
+        for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()) {
+            clearPlayerDataForOffline(sender, offlinePlayer);
         }
         LuckPerms luckPerms = getLuckPerms();
         if (luckPerms != null) {
@@ -75,10 +65,10 @@ public class AdminPlayerData {
         if (offlinePlayer.isOnline()) {
             Player player = offlinePlayer.getPlayer();
             if (player != null) {
-                player.getInventory().clear();
-                player.getEnderChest().clear();
-                player.kickPlayer("§f§lCivEvents §f| §cYour data is being cleared");
+                clearPlayerDataForOnline(sender, player);
             }
+        } else {
+            clearPlayerDataForOffline(sender, offlinePlayer);
         }
         File[] worldFolders = Bukkit.getWorlds().stream()
                 .map(world -> world.getWorldFolder())
@@ -111,5 +101,24 @@ public class AdminPlayerData {
                 sender.sendMessage("§f§lCivEvents §f| §cNo player data found for: " + username);
             }
         }
+    }
+    private static void clearPlayerDataForOnline(CommandSender sender, Player player) {
+        player.getInventory().clear();
+        player.getEnderChest().clear();
+        player.updateInventory();
+        player.kickPlayer("§f§lCivEvents §f| §cYour data is being cleared");
+        sender.sendMessage("§f§lCivEvents §f| §aCleared data for online player: " + player.getName());
+    }
+    private static void clearPlayerDataForOffline(CommandSender sender, OfflinePlayer offlinePlayer) {
+        UUID playerUUID = offlinePlayer.getUniqueId();
+        Bukkit.getScheduler().runTask(Bukkit.getPluginManager().getPlugin("CivEvents"), () -> {
+            Player player = Bukkit.getPlayer(playerUUID);
+            if (player != null) {
+                player.getInventory().clear();
+                player.getEnderChest().clear();
+                player.updateInventory();
+            }
+        });
+        sender.sendMessage("§f§lCivEvents §f| §aCleared data for offline player: " + offlinePlayer.getName());
     }
 }
